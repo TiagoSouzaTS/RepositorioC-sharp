cfd2ea4ed232f1945232832b4863bb687f1b58dd
Tiago Souza <tiagosouza_tp@Hotmail.com>
2023-07-07 17:48:08 -0300

    Finalizando aqui no Ciee

---
+++ Projects/Project3/Entities/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project3.Entities {
+    internal class Post {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+
+    }
+}
--- Projects/Project3/Program.cs
+++ Projects/Project3/Program.cs
@@ -2,25 +2,15 @@
 using System.Collections.Concurrent;
 using System.Globalization;
 using System.Security.Cryptography;
+using Project3.Entities;
 
 namespace Project3
 {
 
     class Program
-    {     // Exemplo de como funciona membros estáticos. 
-
+    {     
         static void Main(string[] args)
         {
-
-            Console.Write($"Entre com o valor do raio: ");
-            double raio = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double circ = Calculadora.Circunferencia(raio);
-            double volume = Calculadora.Volume(raio);
-
-            Console.WriteLine($"A circunferencia é: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
-            Console.WriteLine($"O volume é: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
-            Console.WriteLine($"Valor de é: {Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
 
         }
     }
