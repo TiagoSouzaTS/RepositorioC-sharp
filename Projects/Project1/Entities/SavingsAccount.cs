6cab4ad3cba650267105e6311d41235f0e0cbef0
Tiago Souza <tiagosouza_tp@Hotmail.com>
2023-07-11 12:07:42 -0300

    Adicionando class poupança

---
+++ Projects/Project1/Entities/SavingsAccount.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Project1.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() { 
+        
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance) {
+
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance(double balance) {
+
+            Balance += balance * InterestRate;
+        }
+    }
+}
