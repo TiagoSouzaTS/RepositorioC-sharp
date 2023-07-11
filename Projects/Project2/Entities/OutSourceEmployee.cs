577972036e9338f88e18c61b1cf793bfe73678fd
Tiago Souza <tiagosouza_tp@Hotmail.com>
2023-07-11 20:31:42 -0300

    Exercício employees resolvido

---
+++ Projects/Project2/Entities/OutSourceEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Text;
+
+namespace Project2.Entities
+{
+    internal sealed class OutSourceEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutSourceEmployee() { 
+        
+        }
+
+        public OutSourceEmployee(double additionalCharge, string name, int hours, double valuePerHour) : base(name, hours, valuePerHour) {
+
+            AdditionalCharge = additionalCharge;
+        }
+
+        public sealed override double Payment() {
+
+           double x = base.Payment();
+            return x + ((AdditionalCharge * 110) / 100);
+
+        }
+    }
+}
--- Projects/Project2/Program.cs
+++ Projects/Project2/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Project2.Entities;
+using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
@@ -8,7 +10,43 @@ namespace Project2 {
         
         static void Main(string[] args) {
 
-            
+            Console.Write($"Enter the number of employees: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            List<Employee> employee = new List<Employee>();
+
+            for (int i = 1; i <= n; i++) {
+
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write($"Outsourced (y/n)? ");
+                char outsourced = Convert.ToChar(Console.ReadLine());
+                if (outsourced == 'y') {
+                    Console.Write($"Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write($"Hours: ");
+                    int hours = Convert.ToInt32(Console.ReadLine());
+                    Console.Write($"Value per hour: ");
+                    double valueperhour = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write($"Additional charge: ");
+                    double additionalCharge = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    employee.Add(new OutSourceEmployee { Name = name, Hours = hours, ValuePerHour = valueperhour, AdditionalCharge = additionalCharge});
+                }
+                else {
+                    Console.Write($"Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write($"Hours: ");
+                    int hours = Convert.ToInt32(Console.ReadLine());
+                    Console.Write($"Value per hour: ");
+                    double valueperhour = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    employee.Add(new Employee { Name = name, Hours = hours, ValuePerHour = valueperhour });
+                }
+
+            }
+
+            Console.WriteLine($"PAYMENTS: ");
+            foreach (Employee obj in employee) {
+                Console.WriteLine($"{obj}");
+            }
 
         }
     }
