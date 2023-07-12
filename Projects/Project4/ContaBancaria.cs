67e4b585cd26e8e5998904647a49f8842d3b72c9
Tiago Souza <tiagosouza_tp@Hotmail.com>
2023-07-12 11:36:07 -0300

    Iniciando exercício com métodos e class abstratos

--- Projects/Project4/ContaBancaria.cs
+++ Projects/Project4/ContaBancaria.cs
@@ -7,43 +7,6 @@ namespace Project4
 {
     class ContaBancaria
     {
-        public int NumeroConta { get; private set; }
-        public double DepositoInicial { get; private set; }
-        public double _saldoDaConta { get; private set; }
-        private string _nomeTitular;
-
-
-        public ContaBancaria(int numeroConta, string nomeTitular, double depositoInicial) {
-
-            NumeroConta = numeroConta;
-            _nomeTitular = nomeTitular;
-            DepositoInicial = depositoInicial;
-            Deposito(depositoInicial);
-        }
-
-        public string Nome {
-
-            get { return _nomeTitular; }
-
-            set { 
-
-                if (value != null && value.Length > 0) {
-                    _nomeTitular = value;
-                }
-            }
-        }
-
-        public void Deposito(double deposito) {
-
-            _saldoDaConta += deposito;
-        }
-
-        public void Saque(double saque) {
-            _saldoDaConta -= (saque + 5);
-        }
-
-        public override string ToString() {
-            return ($"A conta de número: {NumeroConta} que pertence ao titular: {_nomeTitular} tem o saldo de: ${_saldoDaConta.ToString("F2", CultureInfo.InvariantCulture)}");
-        }
+        
     }
 }
--- Projects/Project4/Program.cs
+++ Projects/Project4/Program.cs
@@ -10,44 +10,6 @@ namespace Project4
     {   
         static void Main(string[] args)
         {
-            char resultadoDepositoInicial = 'n';
-            double depositoInicial = 0;
-            Console.Write($"Digite o nome do Titular da conta: ");
-            string nome = (Console.ReadLine());
-            Console.Write($"Digite o número da conta: ");
-            int numeroConta = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write($"Você deseja realizar um deposito inicial S/N? ");
-            resultadoDepositoInicial = Convert.ToChar(Console.ReadLine());
-            
-
-            if (resultadoDepositoInicial == 's' || resultadoDepositoInicial == 'S') {
-
-                Console.Write($"Qual a quantidade que deseja depositar inicialmente? ");
-                depositoInicial = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                
-            } else {
-                Console.WriteLine($"Tudo bem, sua conta poderá ser aberta sem um depósito inicial.");
-            }
-
-            ContaBancaria c1 = new ContaBancaria(numeroConta, nome, depositoInicial);
-
-            Console.WriteLine($"{c1}");
-
-            Console.Write($"Digite o valor de depósito que deseja realizar: ");
-            double deposito = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            c1.Deposito(deposito);
-
-            Console.WriteLine($"{c1}");
-
-            Console.Write($"Digite o valor de saque que deseja realizar: ");
-            double saque = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            c1.Saque(saque);
-
-            Console.WriteLine($"{c1}");
-
 
 
         }
