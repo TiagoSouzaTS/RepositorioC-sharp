0f72416ba5e6d0f3ef23e5991aca91af3708dd8e
Tiago Souza <tiagosouza_tp@Hotmail.com>
2023-07-12 20:39:02 -0300

    Exemplo básico de exceções

--- Projects/Project7/Program.cs
+++ Projects/Project7/Program.cs
@@ -5,7 +5,21 @@ namespace Project7 {
 
         static void Main(string[] args) {
 
+            try {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
 
+                int result = n1 / n2;
+                Console.WriteLine($"{result}");
+            } catch (DivideByZeroException apelido) {
+
+                Console.WriteLine($"Error! {apelido.Message}");
+
+            } catch (FormatException apelido2) {
+
+                Console.WriteLine($"Format error {apelido2.Message}");
+            }
+            
 
         }  
     }
