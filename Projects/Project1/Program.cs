d86a957146a0f72b1525057d780e5d0b94f67f4b
Tiago Souza <tiagosouza_tp@Hotmail.com>
2023-07-12 10:36:25 -0300

    Class abstrata permite instanciar lista

--- Projects/Project1/Program.cs
+++ Projects/Project1/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Globalization;
 using System.Runtime.InteropServices;
+using System.Collections.Generic;
 
 
 namespace Project1 {
@@ -9,8 +10,22 @@ namespace Project1 {
     class Program {
         static void Main(string[] args) {
 
-            
-            
+            List<Account> list = new List<Account>();
+
+            list.Add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+
+            list.Add(new BusinessAccount(1002, "Maria", 500.0, 400.0));
+
+            list.Add(new SavingsAccount(1003, "Bob", 500.0, 0.01));
+
+            list.Add(new BusinessAccount(1004, "Anna", 500.0, 500.0));
+            double sum = 0.0;
+
+            foreach(Account account in list) {
+
+                sum += account.Balance;
+            }
+            Console.WriteLine($"Total balance: {sum.ToString("F2", CultureInfo.InvariantCulture)}");
         }
     }
 }
