12a6a73135f07b4f7bf833419b8b21875be6b638
Tiago Souza <tiagosouza_tp@Hotmail.com>
2023-07-13 15:46:34 -0300

    Finalizando exercício sobre exceções completo

--- Projects/Project7/Entities/Reservation.cs
+++ Projects/Project7/Entities/Reservation.cs
@@ -1,10 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Project7.Entities.Exceptions;
 
 namespace Project7.Entities {
     internal class Reservation {
 
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
 
+        public Reservation() {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut) {
+
+            DateTime now = DateTime.Now;
+
+            if (CheckIn < now || checkOut < now) {
+
+                throw new DomainException("Reservation dates for must be future dates");
+
+            }
+            if (checkOut <= checkIn) {
+
+                throw new DomainException("Check-out date must be after ");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration() {
+
+            TimeSpan duration = CheckOut.Subtract(CheckIn); //pegando a diferença da data de saída para a data de entrada.
+
+            return (int)duration.TotalDays; // retornando a diferença em dias, como duration é um double foi necessário realizar um casting. 
+        }
+
+        public void UpdateDates(DateTime chekIn, DateTime checkOut) {
+
+            DateTime now = DateTime.Now;
+
+            if (CheckIn < now || checkOut < now) {
+
+                throw new DomainException("Reservation dates for update must be future dates");
+
+            } 
+            if (checkOut <= CheckIn) {
+
+                throw new DomainException("Check-out for update must be after");
+            }
+
+            CheckIn = chekIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString() {
+
+            return ($"Room {RoomNumber}, check-in: {CheckIn.ToString("dd/MM/yyyy")}, check-out: {CheckOut.ToString("dd/MM/yyyy")}, {Duration()} nights");
+        }
     }
 }
--- Projects/Project7/Program.cs
+++ Projects/Project7/Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using Project7.Entities;
+using System;
+using Project7.Entities.Exceptions;
 
 namespace Project7 {
     class Program {
 
         static void Main(string[] args) {
 
-                 
+            try {
 
-        }  
+                Console.Write($"Room number: ");
+                int roomNumber = Convert.ToInt32(Console.ReadLine());
+                Console.Write($"Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write($"Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation r1 = new Reservation(roomNumber, checkIn, checkOut);
+
+                Console.WriteLine($"{r1}");
+
+                Console.WriteLine();
+
+                Console.WriteLine($"Enter data to update the reservation: ");
+                Console.Write($"Check-in date (dd/MM/yyyy): ");
+                DateTime checkInUpdate = DateTime.Parse(Console.ReadLine());
+                Console.Write($"Check-out date (dd/MM/yyyy): ");
+                DateTime checkOutUpdate = DateTime.Parse(Console.ReadLine());
+
+                r1.UpdateDates(checkInUpdate, checkOutUpdate);
+
+                Console.WriteLine();
+
+                Console.WriteLine($"{r1}");
+
+            } catch(DomainException apelido) {
+
+                Console.WriteLine($"Error inr reservation: {apelido.Message}");
+            } catch(FormatException apelido1) {
+
+                Console.WriteLine($"Format error: {apelido1.Message}");
+            } catch(Exception apelido2) {
+
+                Console.WriteLine($"Unexpected error: {apelido2.Message}");
+            } 
+        }
     }
 }
