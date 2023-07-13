e8523803116ef47d0d5e4709d2c199e4059cbedf
Tiago Souza <tiagosouza_tp@Hotmail.com>
2023-07-13 08:59:35 -0300

    criação da pasta Code_Models

---
+++ Projects/Project7/Entities/Reservation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project7.Entities {
+    internal class Reservation {
+
+
+    }
+}
--- Projects/Project7/Program.cs
+++ Projects/Project7/Program.cs
@@ -5,21 +5,7 @@ namespace Project7 {
 
         static void Main(string[] args) {
 
-            try {
-                int n1 = int.Parse(Console.ReadLine());
-                int n2 = int.Parse(Console.ReadLine());
-
-                int result = n1 / n2;
-                Console.WriteLine($"{result}");
-            } catch (DivideByZeroException apelido) {
-
-                Console.WriteLine($"Error! {apelido.Message}");
-
-            } catch (FormatException apelido2) {
-
-                Console.WriteLine($"Format error {apelido2.Message}");
-            }
-            
+                 
 
         }  
     }
