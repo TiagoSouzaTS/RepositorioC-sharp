ef0325a48ca0be9bc68317fcae5102cc7e2d6eb7
Tiago Souza <tiagosouza_tp@Hotmail.com>
2023-07-17 14:04:24 -0300

    Realizando operações com pastas e arquivos

--- Projects/Project8/Program.cs
+++ Projects/Project8/Program.cs
@@ -1,25 +1,30 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 using System.Net.WebSockets;
 
 namespace Project8 {
     internal class Program {
         static void Main(string[] args) {
 
-            string sourcePath = @"C:\Users\jiraya\Documents\file1.txt"; //utilizado um @ para poder colocar só uma \ para o caminho. 
-            string targetPath = @"C:\Users\jiraya\Documents\file2.txt";
-
+            string path = @"C:\Users\jiraya\Documents\myfolder"; //utilizado um @ para poder colocar só uma \ para o caminho. 
+            
             try {
 
-                string[] lines = File.ReadAllLines(sourcePath);
-
-                using (StreamWriter sw = File.AppendText(targetPath)) {
-
-                    foreach (string line in lines) {
-
-                        sw.WriteLine(line.ToUpper());
-                    }
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine($"FOLDERS: ");
+                foreach ( string item in folders ) {
+                    Console.WriteLine(item);
                 }
+
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine($"FILES: ");
+                foreach (string item in files) {
+                    Console.WriteLine(item);
+                }
+
+                Directory.CreateDirectory(path + @"\newfolder");
+                Directory.CreateDirectory(@"C:\Users\jiraya\Documents\myfolder\newfolder");
             }
             catch (IOException apelido) {
                 Console.WriteLine($"An error ocurred");
