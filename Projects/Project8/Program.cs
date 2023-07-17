ef7a73bd8099ac8558c343ea8a0eb37bf452679a
Tiago Souza <tiagosouza_tp@Hotmail.com>
2023-07-17 14:55:15 -0300

    Finalizando assunto de operações com pastas e arquivos

--- Projects/Project8/Program.cs
+++ Projects/Project8/Program.cs
@@ -7,24 +7,18 @@ namespace Project8 {
     internal class Program {
         static void Main(string[] args) {
 
-            string path = @"C:\Users\jiraya\Documents\myfolder"; //utilizado um @ para poder colocar só uma \ para o caminho. 
-            
+            string path = @"C:\Users\jiraya\Documents\myfolder\file1.txt"; //utilizado um @ para poder colocar só uma \ para o caminho. 
+
             try {
 
-                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
-                Console.WriteLine($"FOLDERS: ");
-                foreach ( string item in folders ) {
-                    Console.WriteLine(item);
-                }
-
-                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
-                Console.WriteLine($"FILES: ");
-                foreach (string item in files) {
-                    Console.WriteLine(item);
-                }
-
-                Directory.CreateDirectory(path + @"\newfolder");
-                Directory.CreateDirectory(@"C:\Users\jiraya\Documents\myfolder\newfolder");
+                Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+                Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+                Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+                Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+                Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+                Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+                Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+                Console.WriteLine("GetTempPath: " + Path.GetTempPath());
             }
             catch (IOException apelido) {
                 Console.WriteLine($"An error ocurred");
