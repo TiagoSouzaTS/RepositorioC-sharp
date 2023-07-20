5428efc7b2f6402440a45a20f006e549661388dd
Tiago Souza <tiagosouza_tp@Hotmail.com>
2023-07-19 21:01:52 -0300

    Finalizando aqui no ciee

---
+++ Projects/Project12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Project12.Entities;
+
+namespace Project12 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            string path = @"C:\Users\jiraya\Documents\myfolder\file1.txt";
+
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream) {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort(); //método para ordenar a lista.
+                    foreach (Employee emp in list) {
+                        Console.WriteLine($"{emp}");
+                    }
+                }
+            }
+            catch (IOException apelido){
+                Console.WriteLine($"An error occurred");
+                Console.WriteLine($"{apelido.Message}");
+            }
+        }
+    }
+}
