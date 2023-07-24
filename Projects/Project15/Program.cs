efc44412d446ada851ac1af5fefc829f10ca770f
Tiago Souza <tiagosouza_tp@Hotmail.com>
2023-07-24 17:46:00 -0300

    Exercício aplicando Equals e GetHashCode manualmente finalizados

---
+++ Projects/Project15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Project15.Entities;
+namespace Project15 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com"};
+            Client b = new Client { Name = "Alex", Email = "maria@gmail.com"};
+            Console.WriteLine(a.Equals(b)); // Vai dar True
+            Console.WriteLine(a == b); // Vai dar False
+            Console.WriteLine(a.GetHashCode()); // vai dar o mesmo código.
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
