39ddf094bb64d2b19b206ebdce942dca2a806d4d
Tiago Souza <tiagosouza_tp@hotmail.com>
2023-07-24 22:58:21 -0300

    Finalizando aqui em casa

---
+++ Projects/Project16/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project16 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            HashSet<string> set = new HashSet<string>();
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (string s in set) {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
