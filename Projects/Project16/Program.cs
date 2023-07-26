703e6bda3f327b644984bf8cd4e5c91764767624
Tiago Souza <tiagosouza_tp@Hotmail.com>
2023-07-26 12:12:24 -0300

    Finalizando aula sobre conjuntos e iniciando exercício

--- Projects/Project16/Program.cs
+++ Projects/Project16/Program.cs
@@ -8,6 +8,12 @@ namespace Project16 {
             SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
             SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
 
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
             PrintCollection(a);
         }
 
